164da49151dbdd321dbe9ea49d9d9da8f7d87a36
chandramerla <Chandra.Merla@ibm.com>
2024-02-13 12:53:02 +0530

    using the CentOS image from /tmp

--- cluster-provision/centos9/scripts/vm.sh
+++ cluster-provision/centos9/scripts/vm.sh
@@ -47,7 +47,7 @@ n="$(printf "%02d" $(( 10#${NODE_NUM} )))"
 cat >/usr/local/bin/ssh.sh <<EOL
 #!/bin/bash
 set -ex
-dockerize -wait tcp://192.168.66.1${n}:22 -timeout 3600s &>/dev/null
+dockerize -wait tcp://192.168.66.1${n}:24 -timeout 3600s &>/dev/null
 ssh -vvv -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@192.168.66.1${n} -i s390x_centos.key -p 22 -q \$@
 EOL
 chmod u+x /usr/local/bin/ssh.sh
@@ -186,7 +186,7 @@ echo "--DEBUG--"
 
 
 qemu_log="qemu_log.txt"
-qemu-system-s390x -enable-kvm -drive format=qcow2,file=box.qcow2,if=virtio,cache=unsafe -machine s390-ccw-virtio -device virtio-net-ccw,netdev=network0,mac=52:55:00:d1:55:${n} -netdev tap,id=network0,ifname=tap01,script=no,downscript=no -device virtio-rng-pci -vnc :01 -cpu host -m 3096M -smp 2 -serial pty -uuid $(cat /proc/sys/kernel/random/uuid)  ${QEMU_ARGS}  >"$qemu_log" 2>&1
+qemu-system-s390x -enable-kvm -drive format=qcow2,file=${next},if=virtio,cache=unsafe -machine s390-ccw-virtio -device virtio-net-ccw,netdev=network0,mac=52:55:00:d1:55:${n} -netdev tap,id=network0,ifname=tap01,script=no,downscript=no -device virtio-rng-pci -vnc :01 -cpu host -m 3096M -smp 2 -serial pty -uuid $(cat /proc/sys/kernel/random/uuid)  ${QEMU_ARGS}  >"$qemu_log" 2>&1
 
 cat "qemu_log.txt"
 #DEBUG
